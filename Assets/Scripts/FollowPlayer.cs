a2be6b590d8cf68565b9b1406e407667461ed51c
Beti4K <beata.kolodziejczyk2004@gmail.com>
2024-02-26 17:34:29 +0000

    Camera offset add
    Adding camera offset

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -12,6 +12,6 @@ public class FollowPlayer : MonoBehaviour
 
     void Update()
     {
-        transform.position = player.transform.position;
+        transform.position = player.transform.position + new Vector3(0, 5, -7);
     }
 }
